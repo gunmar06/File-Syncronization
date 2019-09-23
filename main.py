692ddec6ff7acecc30e01e75dab06b2036c92b23
Xen0Xys <red57101@gmail.com>
2019-09-23 15:38:18 +0200

    Change server and client code

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ s.connect(("8.8.8.8", 80))
 print(s.getsockname()[0])
 s.close()
 """
-
 """
 def segmentMessage(message):
     msg_list = []
@@ -77,29 +76,40 @@ class Server():
     def __init__(self):
         self.ip = self.getIp()
         self.port = 10011
-        self.client_list = {}
+        self.client_list = []
         self.server_on = False
-        self.setupSocket()
-        self.waitConnection()
+        if self.setupSocket():
+            self.sck_buff_g = SocketBufferGestionnary([self.client_list])
+            self.sck_buff_g.start()
+            self.waitConnection()
     def getIp(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("8.8.8.8", 80))
-        ip = s.getsockname()[0]
-        s.close()
-        return ip
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.connect(("8.8.8.8", 80))
+            ip = s.getsockname()[0]
+            s.close()
+            return ip
+        except OSError:
+            return None
     def setupSocket(self):
         try:
             self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.socket.bind((self.ip, self.port))
             self.server_on = True
             self.socket.listen(5)
+            return True
         except PermissionError as e:
             print(e)
+        except TypeError:
+            pass
     def waitConnection(self):
         while self.server_on:
             rlist, _, _ = select.select([self.socket], [], [], 0.05)
             for client in rlist:
-                client.accept()
+                _clt, _ = client.accept()
+                self.client_list.append(_clt)
+            sleep(1/60)
+
 
 class Client():
     def __init__(self):
@@ -107,6 +117,8 @@ class Client():
         self.port = 1001
         self.client_on = False
         self.searchServer()
+        self.sck_buff_g = SocketBufferGestionnary([socket])
+        self.sck_buff_g.start()
     def getIp(self):
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -117,19 +129,22 @@ class Client():
         except OSError as e:
             print(e)
             return None
-        
     def searchServer(self):
-        ip_number = 0
-        while self.client_on == False:
-            separate_ip = self.ip.split(".")
-            new_ip = "{}.{}.{}.{}".format(separate_ip[0], separate_ip[1], separate_ip[2], ip_number)
-            try:
-                self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                self.socket.connect((new_ip, self.port))
-                self.ip = new_ip
-            except ConnectionError as e:
-                print("No connection at {}, try next ({})".format(new_ip, e))
-                ip_number = (ip_number + 1) % 256
+        try:
+            ip_number = 0
+            while self.client_on == False:
+                separate_ip = self.ip.split(".")
+                new_ip = "{}.{}.{}.{}".format(separate_ip[0], separate_ip[1], separate_ip[2], ip_number)
+                try:
+                    self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    self.socket.connect((new_ip, self.port))
+                    self.ip = new_ip
+                    self.client_on = True
+                except ConnectionError as e:
+                    print("No connection at {}, try next ({})".format(new_ip, e))
+                    ip_number = (ip_number + 1) % 256
+        except AttributeError:
+            pass
             
 
             
